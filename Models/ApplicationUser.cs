903585aa4c1f6e2312ced8ad2e5d83383b172730
Dakota Lee <Rendon.S.Ryan@gmail.com>
2022-09-14 18:52:53 -0700

    add Identity to project

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DottiesApothecary.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
